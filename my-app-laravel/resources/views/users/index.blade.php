1dea99cb7509456e96e6943f1cdf7e7a8c09fcd2
Rafaela Rabelo <rafaelarabeloeng@gmail.com>
2022-06-29 17:10:11 -0300

    aula blade

--- my-app-laravel/resources/views/users/index.blade.php
+++ my-app-laravel/resources/views/users/index.blade.php
@@ -1,39 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listagem de Usuários</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-</head>
-<body>
-    <div class="container">
-        <h1>Listagem de Usuários</h1>
-        <table class="table table-hover">
-            <thead class="text-center">
+@extends('template.users')
+@section('title', 'Listagem de Usuários')
+@section('body')
+    <h1>Listagem de Usuários</h1>
+    <table class="table table-hover">
+        <thead class="text-center">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Cadastro</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            @foreach ($users as $user)
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Cadastro</th>
-                    <th scope="col">Ações</th>
+                    <td scope="row">{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ date('d-m-Y - H:i',strtotime($user->created_at)) }}</td>
+                    <td><a href="{{ route('users.show', $user->id)}}" class="btn btn-sm btn-info text-light">Info</a></td>
                 </tr>
-            </thead>
-            <tbody class="text-center">
-                @foreach ($users as $user)
-                    <tr>
-                        <td scope="row">{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ date('d-m-Y - H:i',strtotime($user->created_at)) }}</td>
-                        <td><a href="{{ route('users.show', $user->id)}}" class="btn btn-sm btn-info text-light">Info</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-</body>
-</html>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
--- my-app-laravel/resources/views/users/show.blade.php
+++ my-app-laravel/resources/views/users/show.blade.php
@@ -1,40 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Usuário {{ $user->name }}</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-</head>
-<body>
-    <div class="container">
-        <h1>Usuário {{ $user->name }}</h1>
-        <table class="table table-hover">
-            <thead class="text-center">
+@extends('template.users')
+@section('title', $title)
+@section('body')
+    <h1>Usuário {{ $user->name }}</h1>
+    <table class="table table-hover">
+        <thead class="text-center">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Cadastro</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Cadastro</th>
-                    <th scope="col">Ações</th>
+                    <td scope="row">{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ date('d-m-Y - H:i',strtotime($user->created_at)) }}</td>
+                    <td>
+                        <a href="" class="btn btn-sm btn-warning text-light">Editar</a>
+                        <a href="" class="btn btn-sm btn-danger">Excluir</a>
+                    </td>
                 </tr>
-            </thead>
-            <tbody class="text-center">
-                    <tr>
-                        <td scope="row">{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ date('d-m-Y - H:i',strtotime($user->created_at)) }}</td>
-                        <td>
-                            <a href="" class="btn btn-sm btn-warning text-light">Editar</a>
-                            <a href="" class="btn btn-sm btn-danger">Excluir</a>
-                        </td>
-                    </tr>
-            </tbody>
-        </table>
-    </div>
-</body>
-</html>
+        </tbody>
+    </table>
+@endsection
