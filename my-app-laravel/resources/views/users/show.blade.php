379db3a5ea48f0cc6fe2ab57ca1ba3a0c870a565
Rafaela Rabelo <rafaelarabeloeng@gmail.com>
2022-06-29 15:21:13 -0300

    listagem de usuario

--- my-app-laravel/resources/views/users/show.blade.php
+++ my-app-laravel/resources/views/users/show.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listagem de Usuários</title>
+    <title>Usuário {{ $user->name }}</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
 <body>
     <div class="container">
-        <h1>Listagem de Usuários</h1>
+        <h1>Usuário {{ $user->name }}</h1>
         <table class="table table-hover">
             <thead class="text-center">
                 <tr>
@@ -23,15 +23,16 @@
                 </tr>
             </thead>
             <tbody class="text-center">
-                @foreach ($users as $user)
                     <tr>
                         <td scope="row">{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ date('d-m-Y - H:i',strtotime($user->created_at)) }}</td>
-                        <td><a href="{{ route('users.show', $user->id)}}" class="btn btn-sm btn-info text-light">Info</a></td>
+                        <td>
+                            <a href="" class="btn btn-sm btn-warning text-light">Editar</a>
+                            <a href="" class="btn btn-sm btn-danger">Excluir</a>
+                        </td>
                     </tr>
-                @endforeach
             </tbody>
         </table>
     </div>
