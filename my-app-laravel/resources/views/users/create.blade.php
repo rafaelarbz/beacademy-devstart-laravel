78a24d8a5c0fee7fb62d2fe0716b26c153d85045
Rafaela Rabelo <rafaelarabeloeng@gmail.com>
2022-07-01 21:40:37 -0300

    validações no form

--- my-app-laravel/resources/views/users/create.blade.php
+++ my-app-laravel/resources/views/users/create.blade.php
@@ -2,6 +2,15 @@
 @section('title', 'Novo Usuário')
 @section('body')
     <h1>Novo Usuário</h1>
+
+    @if($errors->any())
+      <div class="alert alert-danger" role="alert">
+          @foreach ($errors->all(); as $error)
+            {{ $error }}<br>
+          @endforeach
+      </div>
+    @endif
+
     <form action="{{ route('users.store') }}" method="POST">
         @csrf
         <div class="mb-3">
